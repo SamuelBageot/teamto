833cb4ce5dd64d3266913839d6e6c18c711008b4
Samuel <samuel.bageot@gmail.com>
2022-11-15 21:35:37 +0100

    add store config

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { Route, Router, Routes } from 'react-router';
-import Home from './Home';
-import './index.css';
-import {createMemoryHistory} from 'history';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { Route, Router, Routes } from "react-router";
+import Home from "./Home";
+import "./index.css";
+import { createMemoryHistory } from "history";
+import { store } from "./redux/store";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 const history = createMemoryHistory();
 
 root.render(
   <React.StrictMode>
     <Router location={""} navigator={history}>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Provider>
     </Router>
-    {/* <Provider store={store}> */}
-    {/* </Provider> */}
   </React.StrictMode>
 );
