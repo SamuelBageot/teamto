8eabeb3272d3d1d8059d153b57b2e3b5cd4ce592
Samuel <samuel.bageot@gmail.com>
2022-11-15 21:32:02 +0100

    add dependencies and configure router

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Route, Router, Routes } from 'react-router';
+import Home from './Home';
 import './index.css';
+import {createMemoryHistory} from 'history';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+const history = createMemoryHistory();
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router location={""} navigator={history}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+    {/* <Provider store={store}> */}
+    {/* </Provider> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
